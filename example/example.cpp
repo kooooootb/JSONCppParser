0cfd2123761e0c56e842c7c04054f1f4a001b285
zavgm <zavgmaxden@mail.ru>
2023-05-20 17:44:16 +0300

    add example

---
+++ example/example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+#include "JSON/Parser.h"
+
+int main(){
+    std::ifstream ifs("example.txt");
+
+    JSON::Parser parser(ifs);
+    std::cout << parser.getRoot();
+
+    return 0;
+}
